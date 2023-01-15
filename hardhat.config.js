724faba4ce7ba5af4b9d0aee02104658141b4c56
Kage <romzhey@gmail.com>
2023-01-15 22:34:19 +0800

    first commit- contract deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,17 @@
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
+    defaultNetwork: "goerli",
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: process.env.GOERLI_RPC_URL,
+        accounts: [process.env.PRIVATE_KEY],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
